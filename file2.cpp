03187a50977d3b999977a9f4e571a3253c139328
muhammadkhalidhidayatullah29-code <muhammadkhalidhidayatullah29@gmail.com>
2026-03-10 20:45:56 +0700

    menambahkan string setiap nilai

--- file2.cpp
+++ file2.cpp
@@ -5,3 +5,15 @@ float Rata_rata(float a, float b)
 {
     return (a + b) / 2;
 }
+
+string Status_lulus(float r)
+{
+    if (r >= 60)
+        return "Lolos";
+    else if (r >= 50)
+        return "Lolos dipertimbangkan";
+    else if (r >= 40)
+        return "Lolos tetapi martikulasi 1 tahun";    
+    else
+        return "Gagal";
+}
