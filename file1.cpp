cf05323c9d2798fb2558f1f4fee0cfde96c1ac20
muhammadkhalidhidayatullah29-code <muhammadkhalidhidayatullah29@gmail.com>
2026-03-10 20:36:49 +0700

    Menambahkan fungsi srand dan menambahkan if

--- file1.cpp
+++ file1.cpp
@@ -4,3 +4,12 @@ using namespace std;
 int main()
 {
     int x;
+
+    srand(time(0));
+    x = rand() % 10;
+
+    if (x == 5)
+    {
+        x = x + 1;
+        cout << "Nilai x Sudah dikoreksi menjadi : " << x << endl;
+    }
